aa856dc5ed8d2545254714a3ffa54b00c9579531
pgamboa322 <pgamboa322@gmail.com>
2022-08-01 17:59:07 -0500

    added questions prompts

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,80 @@
 // TODO: Include packages needed for this application
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const inquirer = require("inquirer");
+const fs = require('fs');
+const util = require('util');
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// TODO: Create an array of questions for user input
+function questions() {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is your application title?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Type a description of your application.'
+        },
+        {
+            type: 'input',
+            name: 'tableContents',
+            message: 'What are the table of contents?'
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'What are the steps needed to install your application?'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'What are the steps to use your apllication?'
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Select license for your application: ',
+            choices: [
+                {
+                    name: 'Apache License 2.0',
+                    value: 'Apache'
+                },
+                {
+                    name: 'MIT License',
+                    value: 'MIT'
+                },
+                {
+                    name: 'GNU GPLv3',
+                    value: 'GPL',
+                }
+            ]
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'Please enter your applications contributors.'
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Add in any test instructions needed for the user.'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is your github profile username?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?'
+        },
+
+    ])
+}
+
 
 // TODO: Create a function to initialize app
 function init() {}
